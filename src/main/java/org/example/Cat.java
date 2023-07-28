c71527b63331b7e48551ea2734a6c1f8ff5ac13f
dimavolo1 <125883080+dimavolo1@users.noreply.github.com>
2023-07-28 15:03:29 +0500

    commit

--- src/main/java/org/example/Cat.java
+++ src/main/java/org/example/Cat.java
@@ -7,14 +7,14 @@ public class Cat {
     private final String text;
     private final String type;
     private final String user;
-    private final String upvotes;
+    private final Integer upvotes;
 
     public Cat(
             @JsonProperty("id") String id,
             @JsonProperty("text") String text,
             @JsonProperty("type") String type,
             @JsonProperty("user") String user,
-            @JsonProperty("upvotes") String upvotes)
+            @JsonProperty("upvotes") Integer upvotes)
     {
         this.id = id;
         this.text = text;
@@ -39,7 +39,7 @@ public class Cat {
         return user;
     }
 
-    public String getUpvotes() {
+    public Integer getUpvotes() {
         return upvotes;
     }
 
